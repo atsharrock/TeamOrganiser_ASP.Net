4d683621c2aa2026fce3038e6b99ae0a43686231
atsharrock <atsharrock@yahoo.co.uk>
2020-04-14 12:43:36 +0100

    Remove modal button bottom of accounts index page

---
+++ TeamOrganiser/Data/Migrations/20200414114000_Added_Players.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TeamOrganiser.Data.Migrations
+{
+    public partial class Added_Players : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FootballPlayer",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CentreBack = table.Column<int>(nullable: false),
+                    Sweeper = table.Column<int>(nullable: false),
+                    FullBack = table.Column<int>(nullable: false),
+                    WingBack = table.Column<int>(nullable: false),
+                    CentreMidfield = table.Column<int>(nullable: false),
+                    DefensiveMidfield = table.Column<int>(nullable: false),
+                    AttackingMidfield = table.Column<int>(nullable: false),
+                    WideMidfield = table.Column<int>(nullable: false),
+                    Forward = table.Column<int>(nullable: false),
+                    CentreForward = table.Column<int>(nullable: false),
+                    Winger = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FootballPlayer", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Player",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    ContactNumber = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Player", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sports",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Sport = table.Column<int>(nullable: false),
+                    PlayerID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sports", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Sports_Player_PlayerID",
+                        column: x => x.PlayerID,
+                        principalTable: "Player",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sports_PlayerID",
+                table: "Sports",
+                column: "PlayerID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FootballPlayer");
+
+            migrationBuilder.DropTable(
+                name: "Sports");
+
+            migrationBuilder.DropTable(
+                name: "Player");
+        }
+    }
+}
