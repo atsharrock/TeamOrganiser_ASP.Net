b1c31e0e0dacc881ef334383805238fca6527196
atsharrock <atsharrock@yahoo.co.uk>
2020-04-29 12:57:26 +0100

    FootballPlayer updates

--- TeamOrganiser.Models/Football/FootballPlayer.cs
+++ TeamOrganiser.Models/Football/FootballPlayer.cs
@@ -11,17 +11,31 @@ namespace TeamOrganiser.Models
         [Key]
         public int ID { get; set; }
 
+        public int Defence { get; set; }
         public int CentreBack { get; set; }
         public int Sweeper { get; set; }
         public int FullBack { get; set; }
         public int WingBack { get; set; }
+        public int Midfield { get; set; }
         public int CentreMidfield { get; set; }
         public int DefensiveMidfield { get; set; }
         public int AttackingMidfield { get; set; }
         public int WideMidfield { get; set; }
+        public int Attack { get; set; }
         public int Forward { get; set; }
         public int CentreForward { get; set; }
         public int Winger { get; set; }
 
+        public FootballPlayer()
+        {
+
+        }
+
+        public FootballPlayer(int defence, int midfield, int attack)
+        {
+            Defence = defence;
+            Midfield = midfield;
+            Attack = attack;
+        }
     }
 }
