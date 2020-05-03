2e65a82f54086d415b25157acd6aa25aa8ad72d4
atsharrock <atsharrock@yahoo.co.uk>
2020-05-04 00:49:58 +0100

    CreatePlayer and its test method added

--- TeamOrganiser.Tests/SportsFactoryTests.cs
+++ TeamOrganiser.Tests/SportsFactoryTests.cs
@@ -10,9 +10,27 @@ namespace TeamOrganiser.Tests
         [TestMethod]
         public void CreateFootballPlayerTest()
         {
-            var player = SportsFactory.createSportsPerson(new FootballPlayer());
+            //var player = SportsFactory.CreateSportsPerson(new FootballPlayer());
 
-            Assert.IsNotNull(player);
+            //Assert.IsNotNull(player);
+        }
+
+        [TestMethod]
+        public void CreatePlayerTest()
+        {
+            Player NewPlayer = new Player()
+            {
+                FirstName = "Andrew",
+                LastName = "Sharrock",
+                Email = "atsharrock@yahoo.co.uk",
+                ContactNumber = null,
+                Football = true
+            };
+
+            Assert.IsNotNull(NewPlayer);
+            Assert.IsTrue(NewPlayer.Football);
+            Assert.IsFalse(NewPlayer.Basketball);
+
         }
     }
 }
--- TeamOrganiser/SportsFactory.cs
+++ TeamOrganiser/SportsFactory.cs
@@ -6,13 +6,31 @@ namespace TeamOrganiser
 {
     public static class SportsFactory
     {
-        public static IPlayer createSportsPerson(IPlayer player)
+        public static Player CreatePlayer(string firstName, string lastName, string email, string contactNumber,
+                        bool football, bool hockey, bool basketball)
+        {
+            Player NewPlayer = new Player()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Email = email ?? null,
+                ContactNumber = contactNumber ?? null,
+                Football = football,
+                Hockey = hockey,
+                Basketball = basketball
+            };
+
+            return NewPlayer;
+        }
+
+        public static IPlayer CreateSportsPerson(Player player, IPlayer sport)
         {
             if (null == player)
             {
                 throw new System.ArgumentException("Player does not exist");
             }
-            Type type = player.GetType();
+
+            Type type = sport.GetType();
 
             if (type.Name.Equals("FootballPlayer"))
             {
@@ -21,5 +39,6 @@ namespace TeamOrganiser
 
             throw new Exception();
         }
+
     }
 }
